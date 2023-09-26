76149fdc27dc282205dc1dd0d01cf3dbef17a75e
Alberto Ortiz <alfaortiz.sis@gmail.com>
2023-09-26 12:15:41 -0400

    ADD cambios en path

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrobuild.tips',
+  site: 'https://github.com/FenixSoft-GitHub',
+  base: '/astrobuild'
   integrations: [tailwind(), mdx(), react(), vue(), svelte(), sitemap()]
 });
